e02f649879fcec16b27f6d2cea0b22d1d27a4eb6
吴凯 马 <1750474571@qq.com>
2026-01-17 02:24:22 +0800

    创建项目，添加桌子并完善切换样式逻辑

---
+++ A04da90a2dc7f464b90982fecedb72818B/apple_furniture_scripts/Client.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from .QuModLibs.Client import *
+
---
+++ A04da90a2dc7f464b90982fecedb72818B/apple_furniture_scripts/Server.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import time
+from .QuModLibs.Server import *
+from .QuModLibs.Util import QThrottle
+
+@Listen(Events.ItemUseOnAfterServerEvent)
+@QThrottle(intervalTime=0.2)
+def change(args):
+    item_dict=args["itemDict"]
+    block_pos=args["x"],args["y"],args["z"]
+    block_name=args["blockName"]
+    dimension=args["dimensionId"]
+    comp=serverApi.GetEngineCompFactory().CreateBlockInfo(serverApi.GetLevelId)
+    # 原版斧子
+    axe_name={
+        'minecraft:axe',
+        'minecraft:wooden_axe',
+        'minecraft:stone_axe',
+        'minecraft:iron_axe',
+        'minecraft:diamond_axe',
+        'minecraft:netherite_axe',
+        'minecraft:golden_axe',
+    }
+    # 方块状态循环映射
+    table_cycle = {
+        'swan_town:table1': 'swan_town:table2',
+        'swan_town:table2': 'swan_town:table3',
+        'swan_town:table3': 'swan_town:table4',
+        'swan_town:table4': 'swan_town:table1'
+    }
+    
+    # 检查当前方块是否在循环映射中
+    if block_name in table_cycle and item_dict['newItemName'] in axe_name:
+        block_dict = {
+            'name': table_cycle[block_name]
+        }
+        comp.SetBlockNew((block_pos), block_dict, 0, dimension)
