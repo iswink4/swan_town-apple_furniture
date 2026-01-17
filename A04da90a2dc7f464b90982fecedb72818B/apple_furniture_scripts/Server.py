d63f2f4f9fb44aaf31f43cd940199ca2c62d2fe7
吴凯 马 <1750474571@qq.com>
2026-01-17 16:17:10 +0800

    添加多面向

--- A04da90a2dc7f464b90982fecedb72818B/apple_furniture_scripts/Server.py
+++ A04da90a2dc7f464b90982fecedb72818B/apple_furniture_scripts/Server.py
@@ -31,7 +31,10 @@ def change(args):
     
     # 检查当前方块是否在循环映射中
     if block_name in table_cycle and item_dict['newItemName'] in axe_name:
+        old_block_dict=comp.GetBlockNew(block_pos,dimension)
+        old_aux=old_block_dict['aux']
         block_dict = {
-            'name': table_cycle[block_name]
+            'name': table_cycle[block_name],
+            'aux': old_aux
         }
         comp.SetBlockNew((block_pos), block_dict, 0, dimension)
