ed034b012eb38add0ec1a13be43aa2590347f0ff
吴凯 马 <1750474571@qq.com>
2026-01-17 15:53:09 +0800

    bug

--- A04da90a2dc7f464b90982fecedb72818B/apple_furniture_scripts/Server.py
+++ A04da90a2dc7f464b90982fecedb72818B/apple_furniture_scripts/Server.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
+
+from A04da90a2dc7f464b90982fecedb72818B.apple_furniture_scripts.QuModLibs.Client import clientApi
 from .QuModLibs.Server import *
 from .QuModLibs.Util import QThrottle
 
@@ -21,7 +23,7 @@ def change(args):
         'minecraft:netherite_axe',
         'minecraft:golden_axe',
     }
-    # 方块状态循环映射
+    # 桌子状态循环映射
     table_cycle = {
         'swan_town:table1': 'swan_town:table2',
         'swan_town:table2': 'swan_town:table3',
@@ -30,8 +32,16 @@ def change(args):
     }
     
     # 检查当前方块是否在循环映射中
-    if block_name in table_cycle and item_dict['newItemName'] in axe_name:
-        block_dict = {
-            'name': table_cycle[block_name]
-        }
-        comp.SetBlockNew((block_pos), block_dict, 0, dimension)
+    #if block_name in table_cycle and item_dict['newItemName'] in axe_name:
+    #    block_dict = {
+    #        'name': table_cycle[block_name]
+    #    }
+    #    comp.SetBlockNew((block_pos), block_dict, 0, dimension)
+@Listen(Events.ServerEntityTryPlaceBlockEvent)
+def offset(args):
+    block_pos=args["x"],args["y"],args["z"]
+    block_name=args["fullName"]
+    dimension=args["dimensionId"]
+    print(block_name)
+
+    
